5ba59dd4b90e4ac262fb604ff2a3a0bcd36babdb
Laberbear <Laberbear@users.noreply.github.com>
2016-10-14 11:44:23 +0200

    - Removed error catching in gather main. Measurement errors should be handled by the specific methods

--- app/gathering/gather_main.py
+++ app/gathering/gather_main.py
@@ -129,12 +129,7 @@ class GatherThread(threading.Thread):
 def getMeasurementAndSend(component, metric, args):
     """ Gets the specified metric from the component and sends it into the queue and data dictionary """
     # Get the data
-    try:
-        newData = getMeasurement(component, metric, args)
-    except Exception as e:
-        log.error(e)
-        log.error("CATCHED MEASUREMENT ERROR")
-        return
+    newData = getMeasurement(component, metric, args)
     # Put that data into the queue
     queue_manager.putMeasurementIntoQueue(component, metric, newData, args)
 
