822149c995f2ea606f50304d2b825e7caebe0dcf
EndzeitBegins <lucas.ottersbach@gmail.com>
2016-10-30 19:35:46 +0100

    added a test case for the unified database interface

---
+++ test/database/test_unified_database_interface.py
@@ -0,0 +1,36 @@
+from unittest import TestCase
+from database.unified_database_interface import UnifiedDatabaseInterface
+
+
+class TestUnifiedDatabaseInterface(TestCase):
+    def test_get_measurement_insertion_transaction(self):
+        from database._measurement_insert_transaction import MeasurementInsertTransaction
+
+        self.assertIsInstance(
+            UnifiedDatabaseInterface().get_measurement_insertion_transaction(),
+            MeasurementInsertTransaction
+        )
+
+    def test_get_measurement_data_reader(self):
+        from database._measurement_data_reader import MeasurementDataReader
+
+        self.assertIsInstance(
+            UnifiedDatabaseInterface().get_measurement_data_reader(),
+            MeasurementDataReader
+        )
+
+    def test_get_user_preferences_writer_reader(self):
+        from database._user_preferences_writer_reader import UserPreferencesWriterReader
+
+        self.assertIsInstance(
+            UnifiedDatabaseInterface().get_user_preferences_writer_reader(),
+            UserPreferencesWriterReader
+        )
+
+    def test_get_component_metrics_writer_reader(self):
+        from database._component_metrics_writer_reader import ComponentMetricsWriterReader
+
+        self.assertIsInstance(
+            UnifiedDatabaseInterface().get_component_metrics_writer_reader(),
+            ComponentMetricsWriterReader
+        )
