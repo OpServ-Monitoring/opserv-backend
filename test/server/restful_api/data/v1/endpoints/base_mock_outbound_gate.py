d143bce77383c2af712f5075b96cf2b9e7e29af4
EndzeitBegins <lucas.ottersbach@gmail.com>
2016-11-08 14:27:55 +0100

    added unittest for further endpoints

--- test/server/restful_api/data/v1/endpoints/base_mock_outbound_gate.py
+++ test/server/restful_api/data/v1/endpoints/base_mock_outbound_gate.py
@@ -31,7 +31,68 @@ class BaseMockOutboundGate(OutboundGateInterface):
     @classmethod
     def get_measurements(cls, component: str, metric: str, argument: str = None, start_time: int = 0,
                          end_time: int = time.time() * 1000, limit: int = 5000) -> Iterable:
-        pass
+        return [
+            {
+                "min": "0 percent",
+                "avg": "5 percent",
+                "max": "10 percent",
+                "timestamp": 1000000
+            },
+            {
+                "min": "10 percent",
+                "avg": "15 percent",
+                "max": "20 percent",
+                "timestamp": 1001000
+            },
+            {
+                "min": "20 percent",
+                "avg": "25 percent",
+                "max": "30 percent",
+                "timestamp": 1002000
+            },
+            {
+                "min": "30 percent",
+                "avg": "35 percent",
+                "max": "40 percent",
+                "timestamp": 1003000
+            },
+            {
+                "min": "40 percent",
+                "avg": "45 percent",
+                "max": "50 percent",
+                "timestamp": 1004000
+            },
+            {
+                "min": "50 percent",
+                "avg": "55 percent",
+                "max": "60 percent",
+                "timestamp": 1005000
+            },
+            {
+                "min": "60 percent",
+                "avg": "65 percent",
+                "max": "70 percent",
+                "timestamp": 1006000
+            },
+            {
+                "min": "70 percent",
+                "avg": "75 percent",
+                "max": "80 percent",
+                "timestamp": 1007000
+            },
+            {
+                "min": "80 percent",
+                "avg": "885 percent",
+                "max": "90 percent",
+                "timestamp": 1008000
+            },
+            {
+                "min": "90 percent",
+                "avg": "95 percent",
+                "max": "100 percent",
+                "timestamp": 1009000
+            }
+        ]
 
     @classmethod
     def set_user_preference(cls, key: str, value: str) -> None:
---
+++ test/server/restful_api/data/v1/endpoints/test_partitions_partition.py
@@ -0,0 +1,146 @@
+from unittest import TestCase
+
+from server.restful_api.data.v1.endpoints.partitions_partition import PartitionsPartitionEndpoint
+from base_mock_outbound_gate import BaseMockOutboundGate
+
+from server.restful_api.general.requestholder import RequestHolder
+
+
+class TestCpucoresCpucore(TestCase):
+    def setUp(self):
+        self.endpoint = PartitionsPartitionEndpoint()
+        self.endpoint.set_outbound_gate(BaseMockOutboundGate)
+
+        self.request = RequestHolder()
+        self.request.set_uri("opserv.org/test/api/data/v1/partitions/id")
+        self.request.set_params({
+            "partition": "id"
+        })
+
+    def test_get_cpu_id_validator(self):
+        validator = self.endpoint.get_partition_id_validator()
+
+        self.assertEqual(
+            validator[0],
+            "partition"
+        )
+
+        validation_func = validator[1]
+
+        self.assertTrue(
+            validation_func("id")
+        )
+
+        self.assertTrue(
+            validation_func("2")
+        )
+
+        self.assertFalse(
+            validation_func(2)
+        )
+
+        self.assertFalse(
+            validation_func("")
+        )
+
+        self.assertFalse(
+            validation_func(None)
+        )
+
+    def test_get_name(self):
+        self.assertEqual(
+            self.endpoint.get_name(),
+            "partition entity"
+        )
+
+    def test_get_paths(self):
+        self.assertEqual(
+            self.endpoint.get_paths(),
+            ["/partitions/<string:partition>"]
+        )
+
+    def test_handle_request_with_method_post(self):
+        self.request.set_http_method(RequestHolder.METHOD_POST())
+
+        response = self.endpoint.handle_request(self.request)
+        self.assertEqual(
+            response.get_status(),
+            400
+        )
+
+    def test_handle_request_with_method_put(self):
+        self.request.set_http_method(RequestHolder.METHOD_PUT())
+
+        response = self.endpoint.handle_request(self.request)
+        self.assertEqual(
+            response.get_status(),
+            400
+        )
+
+    def test_handle_request_with_method_delete(self):
+        self.request.set_http_method(RequestHolder.METHOD_DELETE())
+
+        response = self.endpoint.handle_request(self.request)
+        self.assertEqual(
+            response.get_status(),
+            400
+        )
+
+    def test_handle_request_with_method_get_and_invalid_params(self):
+        self.request.set_http_method(RequestHolder.METHOD_GET())
+        self.request.set_params({
+            "partition": None
+        })
+
+        response = self.endpoint.handle_request(self.request)
+        self.assertEqual(
+            response.get_status(),
+            400
+        )
+
+    def test_handle_request_with_method_get_and_without_params(self):
+        self.request.set_http_method(RequestHolder.METHOD_GET())
+        self.request.set_params({})
+
+        response = self.endpoint.handle_request(self.request)
+        self.assertEqual(
+            response.get_status(),
+            400
+        )
+
+    def test_handle_request_with_method_get(self):
+        self.request.set_http_method(RequestHolder.METHOD_GET())
+
+        response = self.endpoint.handle_request(self.request)
+
+        self.assertEqual(
+            response.get_status(),
+            200
+        )
+
+        response_body = response.get_body()
+        self.assertIn(
+            "links",
+            response_body
+        )
+
+        response_body_links = response_body["links"]
+        self.assertIn(
+            "self",
+            response_body_links,
+        )
+
+        self.assertIn(
+            "parent",
+            response_body_links
+        )
+
+        self.assertIn(
+            "children",
+            response_body_links
+        )
+
+        self.assertEqual(
+            len(response_body_links["children"]),
+            3
+        )
