2e78ec55d6d0e3bbd7c6c130b392b0aec9100b6b
lucas_ottersbach <lucas.ottersbach@googlemail.com>
2017-01-10 10:45:57 +0100

    minor database fix

--- app/database/helper/database_connection_helper.py
+++ app/database/helper/database_connection_helper.py
@@ -8,8 +8,8 @@ class DatabaseConnectionHelper:
     def retrieve_database_connection(cls):
         connection = sqlite3.connect(cls._location)
 
-        connection.execute("PRAGMA FOREIGN_KEYS=ON")
         connection.execute("PRAGMA JOURNAL_MODE=WAL")
+        connection.execute("PRAGMA FOREIGN_KEYS=ON")
         connection.commit()
 
         return connection
