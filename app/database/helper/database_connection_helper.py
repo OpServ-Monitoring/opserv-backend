9d47a876aa9c342be8075186ad0aa8899436ebf1
EndzeitBegins <lucas.ottersbach@googlemail.com>
2017-02-06 16:59:59 +0100

    Fixing issue #19 by changing isolation_level during PRAGMAs
    Removal of 3.5-dev from TravisCI build as 3.6 is released already

--- app/database/helper/database_connection_helper.py
+++ app/database/helper/database_connection_helper.py
@@ -15,14 +15,16 @@ class DatabaseConnectionHelper:
             log.error("Could not connect to the database.")
             raise err
 
-        connection.commit()
-        with connection:
-            connection.execute("PRAGMA JOURNAL_MODE=WAL")
+        # Fix for a bug occurring in Python 3.6 on OSX, see issue #19 on Github
+        old_isolation_level = connection.isolation_level  # Save the default isolation level
+        connection.isolation_level = None  # Set the isolation level to None, to avoid "within transaction" bug
 
-        # connection.execute("PRAGMA JOURNAL_MODE=WAL")
+        connection.execute("PRAGMA JOURNAL_MODE=WAL")
         connection.commit()
         connection.execute("PRAGMA FOREIGN_KEYS=ON")
         connection.commit()
+
+        connection.isolation_level = old_isolation_level  # Reset to the original isolation level
 
         log.debug("Database connection established.")
 
